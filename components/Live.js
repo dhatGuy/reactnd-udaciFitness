ecca15c4893b0b9e597ef0065cf55c5ccdc94036
dhatGuy <odunsiolakunbi@gmail.com>
2020-08-13 10:48:13 +0100

    geolocation finishing

--- components/Live.js
+++ components/Live.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import { purple, white } from "../utils/colors";
 import { Foundation } from "@expo/vector-icons";
+import { calculateDirection } from "../utils/helpers";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 const Container = styled.View`
   flex: 1;
@@ -25,13 +28,15 @@ const Metric = styled.View`
 const Header = styled.Text`
   font-size: 35px;
   text-align: center;
-  color: ${(props)=> props.color ? white : "black"};
+  /* color: ${(props) =>
+    props.color}; I have no idea why this gives an error 😕😕 */
+  color: ${(props) => (props.color ? white : "black")};
 `;
 const SubHeader = styled.Text`
   font-size: 25px;
   text-align: center;
   margin-top: 5px;
-  color: ${(props)=> props.color}
+  color: ${(props) => props.color}; /* And it work here 😂😂 */
 `;
 
 const DirectionContainer = styled.View`
@@ -67,11 +72,45 @@ const ButtonText = styled.Text`
 class Live extends Component {
   state = {
     coords: null,
-    status: "ddd",
+    status: null,
     direction: "",
   };
 
-  askPermission = () => {};
+  componentDidMount() {
+    this.askPermission();
+  }
+  askPermission = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === "granted") {
+          return this.setLocation();
+        }
+        this.setState(() => ({ status }));
+      })
+      .catch((error) =>
+        console.warn("Error asking Location Permission: ", error)
+      );
+  };
+
+  setLocation = () => {
+    Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        timeInterval: 1,
+        distanceInterval: 1,
+      },
+      ({ coords }) => {
+        const newDirection = calculateDirection(coords.heading);
+        const { direction } = this.state;
+
+        this.setState(() => ({
+          coords,
+          direction: newDirection,
+          status: "granted",
+        }));
+      }
+    );
+  };
   render() {
     const { coords, status, direction } = this.state;
 
@@ -103,16 +142,20 @@ class Live extends Component {
           <Container>
             <DirectionContainer>
               <Header>You are heading</Header>
-              <Direction>West</Direction>
+              <Direction>{direction}</Direction>
             </DirectionContainer>
             <MetricContainer>
               <Metric>
                 <Header color>Altitude</Header>
-                <SubHeader color={white}>{200} Feet</SubHeader>
+                <SubHeader color={white}>
+                  {Math.round(coords.altitude * 3.2808)} Feet
+                </SubHeader>
               </Metric>
               <Metric>
                 <Header color>Speed</Header>
-                <SubHeader color={white}>{350} MPH</SubHeader>
+                <SubHeader color={white}>
+                  {(coords.speed * 2.2369).toFixed(1)} MPH
+                </SubHeader>
               </Metric>
             </MetricContainer>
           </Container>
@@ -120,5 +163,4 @@ class Live extends Component {
     }
   }
 }
-
 export default Live;
